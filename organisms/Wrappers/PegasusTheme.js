52b1dd99b2a6513c6ff3beab0563495605c9ae92
Rodrigo <rodrigosedano@me.com>
2022-06-26 02:02:24 +0200

    Themes

--- organisms/Wrappers/PegasusTheme.js
+++ organisms/Wrappers/PegasusTheme.js
@@ -7,6 +7,9 @@ import Header from 'components/organisms/Header/Header.js';
 import Aside from "components/organisms/Aside/Aside.js";
 import Main from "components/organisms/Main/Main.js";
 
+import darkNoir from "assets/darkNoir.jpg";
+import rbsimple from "assets/ES-DE_01.png";
+import modern from "assets/ES-DE_01.png";
 
 import {
   BtnSimple,
@@ -32,20 +35,55 @@ const PegasusTheme = () => {
     disabledBack: false,
   });
   const { disabledNext, disabledBack } = statePage;
-
+  const bezelsSet = (bezelStatus) => {
+    console.log('hi')
+  };
   return (
     <>
       {/*  <ExploreContainer name="Tab 1 page" /> */}
       <div className="app">
-        <Aside />
-
         <div className="wrapper">
-          <Header title="Choose your default" bold="Theme" />
+          <Header title="Choose your default EmulationStation DE" bold="Theme" />
           <Main>
-            <p className="lead">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-              quis sagittis orci, vel scelerisque tortor.
-            </p>
+          <div className="steps steps--nowrap">
+            <input
+              type="radio"
+              id="43"
+              name="device"
+              onChange={() => bezelsSet(true)}
+            />
+            <label for="43" className="step step--bezel">
+              <div className="step-img">
+                <img src={darkNoir} alt="Background" />
+              </div>
+              <figcaption>Epic Noir</figcaption>
+            </label>
+            <input
+              type="radio"
+              id="32"
+              name="device"
+              onChange={() => bezelsSet(false)}
+            />
+            <label for="32" className="step step--bezel">
+              <div className="step-img">
+                <img src={rbsimple} alt="Background" />
+              </div>
+              <figcaption>RB Simple </figcaption>
+            </label>  
+            <input
+              type="radio"
+              id="169"
+              name="device"
+              onChange={() => bezelsSet(false)}
+            />
+            <label for="169" className="step step--bezel">
+              <div className="step-img">
+                <img src={modern} alt="Background" />
+              </div>
+              <figcaption>Modern </figcaption>
+            </label>        
+                  
+          </div>
           </Main>
           <Footer
             back="aspect-ratio-3d"
