ddaf5240016f964b4d49648efad0be717fbaa560
Rodrigo <rodrigosedano@me.com>
2022-07-12 21:56:11 +0200

    debug mode

--- organisms/Aside/Aside.js
+++ organisms/Aside/Aside.js
@@ -1,11 +1,22 @@
-import React, { useEffect, useState, useContext } from "react";
-//import { GlobalContext } from "../context/globalContext";
-import bgImg from "assets/bg.gif";
-import "./Aside.scss"
+import React, { useEffect, useState, useContext } from 'react';
+import { GlobalContext } from 'context/globalContext';
+import bgImg from 'assets/bg.gif';
+import './Aside.scss';
 
 const Aside = ({ props }) => {
+  const { state, setState } = useContext(GlobalContext);
+  const { debug } = state;
+
+  const toggleDebug = () => {
+    setState({
+      ...state,
+      debug: !debug,
+    });
+  };
+
   return (
     <aside>
+      <div className="aside-debug" onClick={toggleDebug}></div>
       <img src={bgImg} alt="Background" />
     </aside>
   );
--- organisms/Header/Header.js
+++ organisms/Header/Header.js
@@ -1,11 +1,18 @@
 import React, { useEffect, useState, useContext } from 'react';
-import "./Header.scss"
+import { GlobalContext } from 'context/globalContext';
+import './Header.scss';
 const Header = ({ title, bold }) => {
+  const { state, setState } = useContext(GlobalContext);
+  const { debug, debugText } = state;
+
   return (
     <header className="header">
-      <h1 className="h2">
-        {title} <span>{bold}</span>
-      </h1>
+      {!debug && (
+        <h1 className="h2">
+          {title} <span>{bold}</span>
+        </h1>
+      )}
+      {debug && <code>{JSON.stringify(debugText)}</code>}
     </header>
   );
 };
