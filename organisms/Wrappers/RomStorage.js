5f854773c7ff87f3d65cbfc37b84a899c644f713
Rodrigo <rodrigosedano@me.com>
2022-07-17 00:17:11 +0200

    fixes SDCard

--- organisms/Wrappers/RomStorage.js
+++ organisms/Wrappers/RomStorage.js
@@ -37,7 +37,7 @@ const RomStorage = ({
               sure you formatted it on Gaming Mode
             </p>
             <div className="cards">
-              {sdCardValid && (
+              {sdCardValid === true && (
                 <Card
                   css={storage == 'SD-Card' && 'is-selected'}
                   onClick={() => onClick('SD-Card')}
