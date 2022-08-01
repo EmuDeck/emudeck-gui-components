0ac22121c72ccfd024a8c60346a99bb8cc5c0608
Rodrigo <rodrigosedano@me.com>
2022-08-01 11:19:15 +0200

    Update Emus

--- organisms/Wrappers/UpdateEmus.js
+++ organisms/Wrappers/UpdateEmus.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { GlobalContext } from 'context/globalContext';
-import { useNavigate } from 'react-router-dom';
+
 import Footer from 'components/organisms/Footer/Footer.js';
 import Header from 'components/organisms/Header/Header.js';
 import Aside from 'components/organisms/Aside/Aside.js';
@@ -16,79 +16,50 @@ import { Form } from 'getbasecore/Molecules';
 
 import Card from 'components/molecules/Card/Card.js';
 
-import ToolsAndStuffImg from 'assets/powertools.png';
+import UpdateEmusImg from 'assets/powertools.png';
 
-const ToolsAndStuff = ({
+const UpdateEmus = ({
   disabledNext,
   disabledBack,
   downloadComplete,
-  onChange,
-  onClick,
+  onClickDiscover,
+  onClickAppImage,
   next,
   back,
   hasSudo,
   nextText,
 }) => {
   const { state, setState } = useContext(GlobalContext);
-  const { achievements } = state;
-  const navigate = useNavigate();
-  const goTo = (href) => {
-    navigate('/' + href);
-  };
+  const { sudoPass, UpdateEmus } = state;
+
   return (
     <div className="app">
-      <Aside />
+    <Aside />
       <div className="wrapper">
-        <Header title="Tools &" bold="Stuff" />
+        <Header title="Update your" bold="Emulators" />
         <Main>
           <p className="lead">
-            In this section you'll find tools and scripts that will allow you to get the most of your Device.
+            We use two types of emulators. Some are installed from the Discover Store ( Flatpaks ), and others downloaded directly from the developer's website ( AppImages ).
           </p>
-          <div>
+          <p>Choose what do you want to update</p>
           <BtnSimple
             css="btn-simple--1"
             type="button"
-            onClick={() => goTo('power-tools')}
+            aria="Update Emulators from the Discover Store"
+            onClick={()=> onClickDiscover() }
+            disabled={disabledNext && 'true'}
           >
-            PowerTools
+            Update Flatpak Emulators
           </BtnSimple>
-
           <BtnSimple
             css="btn-simple--1"
             type="button"
-            onClick={() => goTo('gyrodsu')}
-
+            aria="Update appImages"
+            onClick={()=> onClickAppImage() }
+            disabled={disabledNext && 'true'}
           >
-            GyroDSU
+           Update AppImages Emulators
           </BtnSimple>
-
-          <BtnSimple
-            css="btn-simple--1"
-            type="button"
-            onClick={() => goTo('chd-tool')}
-
-          >
-            CHD Compressor
-          </BtnSimple>
-
-
-          <BtnSimple
-            css="btn-simple--1"
-            type="button"
-            onClick={() => goTo('update-emulators')}
-          >
-            Update Emulators
-          </BtnSimple>
-        </div>
-          <BtnSimple
-            css="btn-simple--3"
-            type="button"
-            onClick={() => goTo('uninstall')}
-          >
-            Uninstall EmuDeck
-          </BtnSimple>
-
-
         </Main>
         <Footer
           next={false}
@@ -101,4 +72,4 @@ const ToolsAndStuff = ({
   );
 };
 
-export default ToolsAndStuff;
+export default UpdateEmus;
