dc2c304e9a07915e08b81987cf437bb4c73fc305
Rodrigo <rodrigosedano@me.com>
2022-09-20 16:45:55 +0200

    cloud fixes

--- organisms/Wrappers/CloudSync.js
+++ organisms/Wrappers/CloudSync.js
@@ -47,63 +47,56 @@ const CloudSync = ({
             the go on your Deck
           </p>
 
-          <div className="container--grid">
-            <div data-col-sm="7">
-              <p>Select one of the following providers:</p>
+          <p>Select one of the following providers:</p>
 
-              <div className="cards cards--mini">
-                <Card
-                  css={cloudSync == 'box' && 'is-selected'}
-                  onClick={() => onClick('box')}
-                >
-                  <img src={box} alt="Box" />
-                  <span className="h6">Box</span>
-                </Card>
-                <Card
-                  css={cloudSync == 'dropbox' && 'is-selected'}
-                  onClick={() => onClick('dropbox')}
-                >
-                  <img src={dropbox} alt="dropbox" />
-                  <span className="h6">DropBox</span>
-                </Card>
-                <Card
-                  css={cloudSync == 'gdrive' && 'is-selected'}
-                  onClick={() => onClick('gdrive')}
-                >
-                  <img src={gdrive} alt="gdrive" />
-                  <span className="h6">Google Drive</span>
-                </Card>
-                <Card
-                  css={cloudSync == 'nextcloud' && 'is-selected'}
-                  onClick={() => onClick('nextcloud')}
-                >
-                  <img src={nextcloud} alt="nextcloud" />
-                  <span className="h6">NextCloud</span>
-                </Card>
-                <Card
-                  css={cloudSync == 'onedrive' && 'is-selected'}
-                  onClick={() => onClick('onedrive')}
-                >
-                  <img src={onedrive} alt="onedrive" />
-                  <span className="h6">OneDrive</span>
-                </Card>
-              </div>
-              {cloudSync !== '' && cloudSync !== false && (
-                <BtnSimple
-                  css="btn-simple--1"
-                  type="button"
-                  aria="Install SaveSync"
-                  onClick={() => onClick()}
-                  disabled={disabledNext && 'true'}
-                >
-                  Install SaveSync
-                </BtnSimple>
-              )}
-            </div>
-            <div data-col-sm="5">
-              <img src={cloudSyncOn} alt="bg" />
-            </div>
+          <div className="cards cards--mini">
+            <Card
+              css={cloudSync == 'box' && 'is-selected'}
+              onClick={() => onClick('box')}
+            >
+              <img src={box} alt="Box" />
+              <span className="h6">Box</span>
+            </Card>
+            <Card
+              css={cloudSync == 'dropbox' && 'is-selected'}
+              onClick={() => onClick('dropbox')}
+            >
+              <img src={dropbox} alt="dropbox" />
+              <span className="h6">DropBox</span>
+            </Card>
+            <Card
+              css={cloudSync == 'gdrive' && 'is-selected'}
+              onClick={() => onClick('gdrive')}
+            >
+              <img src={gdrive} alt="gdrive" />
+              <span className="h6">Google Drive</span>
+            </Card>
+            <Card
+              css={cloudSync == 'nextcloud' && 'is-selected'}
+              onClick={() => onClick('nextcloud')}
+            >
+              <img src={nextcloud} alt="nextcloud" />
+              <span className="h6">NextCloud</span>
+            </Card>
+            <Card
+              css={cloudSync == 'onedrive' && 'is-selected'}
+              onClick={() => onClick('onedrive')}
+            >
+              <img src={onedrive} alt="onedrive" />
+              <span className="h6">OneDrive</span>
+            </Card>
           </div>
+          {cloudSync !== '' && cloudSync !== false && (
+            <BtnSimple
+              css="btn-simple--1"
+              type="button"
+              aria="Install SaveSync"
+              onClick={() => onClick()}
+              disabled={disabledNext && 'true'}
+            >
+              Install SaveSync
+            </BtnSimple>
+          )}
         </Main>
         <Footer
           next={false}
