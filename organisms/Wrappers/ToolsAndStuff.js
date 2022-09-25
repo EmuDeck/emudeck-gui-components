e20bc719207b7f671966c5e45b941ea6067d1351
Rodrigo <rodrigosedano@me.com>
2022-09-25 23:22:04 +0200

    Better Zenity

--- organisms/Wrappers/ToolsAndStuff.js
+++ organisms/Wrappers/ToolsAndStuff.js
@@ -37,7 +37,7 @@ const ToolsAndStuff = ({
   const ipcChannel = window.electron.ipcRenderer;
   const openSRM = () => {
     ipcChannel.sendMessage('bash', [
-      `zenity --question --width 450 --title \"Close Steam/Steam Input?\" --text \"Exit Steam to launch Steam Rom Manager? Desktop controls will temporarily revert to touch/trackpad/L2/R2\" && (kill -15 \$(pidof steam) & ${storagePath}/Emulation/tools/srm/Steam-ROM-Manager.AppImage)`,
+      `zenity --question --width 450 --title "Close Steam/Steam Input?" --text "$(printf "<b>Exit Steam to launch Steam Rom Manager? </b>\n\n To add your Emulators and EmulationStation-DE to steam hit Preview, then Generate App List, then wait for the images to download\n\nWhen you are happy with your image choices hit Save App List and wait for it to say it's completed.\n\nDesktop controls will temporarily revert to touch/trackpad/L2/R2")" && (kill -15 $(pidof steam) & ${storagePath}/Emulation/tools/srm/Steam-ROM-Manager.AppImage)`,
     ]);
   };
 
