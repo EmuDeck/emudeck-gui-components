af76997515c46fac1d2bf3be1259c6f966205830
Rodrigo <rodrigosedano@me.com>
2023-01-10 20:25:46 +0100

    Confirmation page

---
+++ organisms/Wrappers/Confirmation.js
@@ -0,0 +1,127 @@
+import React, { useEffect, useState, useContext } from 'react';
+
+import { GlobalContext } from 'context/globalContext';
+
+import Footer from 'components/organisms/Footer/Footer.js';
+import Header from 'components/organisms/Header/Header.js';
+import Aside from 'components/organisms/Aside/Aside.js';
+import Main from 'components/organisms/Main/Main.js';
+
+import Card from 'components/molecules/Card/Card.js';
+import SelectorMenu from 'components/molecules/SelectorMenu/SelectorMenu.js';
+
+import imgYES from 'assets/HomebrewGamesYES.png';
+import imgNO from 'assets/HomebrewGamesNO.png';
+
+const Confirmation = ({
+  disabledNext,
+  disabledBack,
+  downloadComplete,
+  onClick,
+  next,
+  back,
+  data,
+}) => {
+  const { state, setState } = useContext(GlobalContext);
+  const {
+    mode,
+    storagePath,
+    installEmus,
+    overwriteConfigEmus,
+    autosave,
+    achievements,
+    bezels,
+    ar,
+    shaders,
+    theme,
+    homebrewGames,
+  } = state;
+  const installEmusArray = Object.values(installEmus);
+  const overwriteConfigEmusArray = Object.values(overwriteConfigEmus);
+  return (
+    <>
+      {/*  <ExploreContainer name="Tab 1 page" /> */}
+      <div className="app">
+        <div className="wrapper">
+          <Header title="Here's what we'll do" />
+          <Main>
+            <div className="container--grid">
+              <div data-col-sm="3">
+                <span class="h5">We will install:</span>
+                <ul>
+                  {installEmusArray.map((item) => {
+                    return (
+                      <li>
+                        {item.status ? `✅` : `❌`} - {item.name}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+              <div data-col-sm="3">
+                <span class="h5">We will configure:</span>
+                <ul>
+                  {overwriteConfigEmusArray.map((item) => {
+                    return (
+                      <li>
+                        {item.status ? `✅` : `❌`} - {item.name}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+              <div data-col-sm="3">
+                <span class="h5">Customization:</span>
+                {/*
+                achievements,
+                bezels,
+                ar,
+                shaders,
+                theme,
+                homebrewGames,
+
+                */}
+                <ul>
+                  <li>{autosave ? `✅` : `❌`} - AutoSave </li>
+                  <li>{achievements.name ? `✅` : `❌`} - RetroAchievements</li>
+                  <li>{bezels ? `✅` : `❌`} - Bezels</li>
+                  <li>{shaders.handhelds ? `✅` : `❌`} - Handhelds Shader:</li>
+                  <li>{shaders.classic ? `✅` : `❌`} - Classic 2D Shader</li>
+                  <li>{shaders.classic3d ? `✅` : `❌`} - Classic 3D Shader</li>
+                  <li>{homebrewGames ? `✅` : `❌`} - HomeBrew Games</li>
+                  <li>EmulationStation DE Theme: {theme}</li>
+                  <li>
+                    Sega Classic AR: <strong>{ar.sega}</strong>
+                  </li>
+                  <li>
+                    Nintendo Classic AR: <strong>{ar.snes}</strong>
+                  </li>
+                  <li>
+                    Classic 3D Games AR: <strong>{ar.classic3d}</strong>
+                  </li>
+                  <li>
+                    Gamecube AR: <strong>{ar.dolphin}</strong>
+                  </li>
+                </ul>
+              </div>
+              <div data-col-sm="3">
+                <span class="h5">Installation Path:</span>
+                <ul>
+                  <li>{storagePath}</li>
+                </ul>
+              </div>
+            </div>
+          </Main>
+          <Footer
+            next="confirmation"
+            nextText="Finish"
+            disabledNext={disabledNext}
+            disabledBack={disabledBack}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Confirmation;
