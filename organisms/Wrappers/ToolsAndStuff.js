a649fb4886bb9ea78f91a474405a7ab75c5ab459
Rodrigo <rodrigosedano@me.com>
2023-01-27 15:12:00 +0100

    windows SRM

--- organisms/Wrappers/ToolsAndStuff.js
+++ organisms/Wrappers/ToolsAndStuff.js
@@ -37,9 +37,15 @@ const ToolsAndStuff = ({
   };
   const ipcChannel = window.electron.ipcRenderer;
   const openSRM = () => {
-    ipcChannel.sendMessage('bash', [
-      `zenity --question --width 450 --title "Close Steam/Steam Input?" --text "$(printf "<b>Exit Steam to launch Steam Rom Manager? </b>\n\n To add your Emulators and EmulationStation-DE to steam hit Preview, then Generate App List, then wait for the images to download\n\nWhen you are happy with your image choices hit Save App List and wait for it to say it's completed.\n\nDesktop controls will temporarily revert to touch/trackpad/L2/R2")" && (kill -15 $(pidof steam) & ${storagePath}/Emulation/tools/srm/Steam-ROM-Manager.AppImage)`,
-    ]);
+    if (system === 'win32') {
+      ipcChannel.sendMessage('bash', [
+        `cd ${storagePath} && cd Emulation && cd tools && start srm.exe`,
+      ]);
+    } else {
+      ipcChannel.sendMessage('bash', [
+        `zenity --question --width 450 --title "Close Steam/Steam Input?" --text "$(printf "<b>Exit Steam to launch Steam Rom Manager? </b>\n\n To add your Emulators and EmulationStation-DE to steam hit Preview, then Generate App List, then wait for the images to download\n\nWhen you are happy with your image choices hit Save App List and wait for it to say it's completed.\n\nDesktop controls will temporarily revert to touch/trackpad/L2/R2")" && (kill -15 $(pidof steam) & ${storagePath}/Emulation/tools/srm/Steam-ROM-Manager.AppImage)`,
+      ]);
+    }
   };
 
   const openCSM = () => {
