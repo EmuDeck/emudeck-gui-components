acf86d3dceab6a6fca2466920f896a6916f2b08f
Rodrigo <rodrigosedano@me.com>
2023-02-14 23:22:46 +0100

    FooterIonic

--- organisms/Footer/FooterIonic.jsx
+++ organisms/Footer/FooterIonic.jsx
@@ -1,8 +1,8 @@
-import { useHistory } from 'react-router-dom';
-import { PropTypes } from 'prop-types';
-import { BtnSimple } from 'getbasecore/Atoms';
+import { useHistory } from "react-router-dom";
+import { PropTypes } from "prop-types";
+import { BtnSimple } from "getbasecore/Atoms";
 
-import './Footer.scss';
+import "./Footer.scss";
 
 const FooterIonic = ({
   back,
@@ -34,28 +34,17 @@ const FooterIonic = ({
           type="button"
           aria="Go Back"
           disabled={disabledNext && true}
-          onClick={() => CloseApp()}
-        >
+          onClick={() => CloseApp()}>
           Exit To Gaming Mode
         </BtnSimple>
       )}
       {!!fourth && (
-        <BtnSimple
-          css="btn-simple--2"
-          type="button"
-          onClick={() => CloseApp()}
-          aria="Go Back"
-        >
+        <BtnSimple css="btn-simple--2" type="button" onClick={() => CloseApp()} aria="Go Back">
           {fourthText}
         </BtnSimple>
       )}
       {!!third && (
-        <BtnSimple
-          css="btn-simple--2"
-          type="button"
-          onClick={() => goTo(third)}
-          aria="Go Back"
-        >
+        <BtnSimple css="btn-simple--2" type="button" onClick={() => goTo(third)} aria="Go Back">
           {thirdText}
         </BtnSimple>
       )}
@@ -63,13 +52,10 @@ const FooterIonic = ({
         <BtnSimple
           css="btn-simple--2"
           type="button"
-          onClick={
-            back ? () => goTo(back) : () => this.context.router.history.goBack
-          }
+          onClick={back ? () => goTo(back) : () => history.goBack()}
           aria="Go Back"
-          disabled={disabledBack && true}
-        >
-          {!backText && 'Go Back'}
+          disabled={disabledBack && true}>
+          {!backText && "Go Back"}
           {backText}
         </BtnSimple>
       )}
@@ -80,9 +66,8 @@ const FooterIonic = ({
           type="button"
           onClick={() => goTo(next)}
           aria="Go Next"
-          disabled={disabledNext && true}
-        >
-          {!nextText && 'Continue '}
+          disabled={disabledNext && true}>
+          {!nextText && "Continue "}
           {nextText}
           <svg
             className="rightarrow"
@@ -90,8 +75,7 @@ const FooterIonic = ({
             height="32"
             viewBox="0 0 32 32"
             fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
+            xmlns="http://www.w3.org/2000/svg">
             <path
               fill="currentColor"
               d="M16.4091 8.48003L21.5024 13.5734L1.98242 13.5734L1.98242 18.0178H21.5024L16.4091 23.1111L19.5558 26.2578L30.018 15.7956L19.5558 5.33337L16.4091 8.48003Z"
@@ -119,15 +103,15 @@ FooterIonic.propTypes = {
   exit: PropTypes.string,
 };
 FooterIonic.defaultProps = {
-  back: '',
-  next: '',
-  third: '',
-  fourth: '',
-  fourthText: '',
-  disabledNext: '',
-  disabledBack: '',
-  nextText: '',
-  backText: '',
-  thirdText: '',
-  exit: '',
+  back: "",
+  next: "",
+  third: "",
+  fourth: "",
+  fourthText: "",
+  disabledNext: "",
+  disabledBack: "",
+  nextText: "",
+  backText: "",
+  thirdText: "",
+  exit: "",
 };
