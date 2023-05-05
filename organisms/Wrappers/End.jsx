508973981981b03e0b5e27b521f60f96dfce5f29
Rodrigo <rodrigosedano@me.com>
2023-05-06 01:32:35 +0200

    Endpage for win32

--- organisms/Wrappers/End.jsx
+++ organisms/Wrappers/End.jsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState, useContext } from 'react';
 
 import { GlobalContext } from 'context/globalContext';
-
+import Header from 'components/organisms/Header/Header';
 import Main from 'components/organisms/Main/Main';
 import Card from 'components/molecules/Card/Card';
 import CardSettings from 'components/molecules/CardSettings/CardSettings';
 
 import SimpleCarousel from 'components/molecules/SimpleCarousel/SimpleCarousel';
-import { Img } from 'getbasecore/Atoms';
-import { ProgressBar, BtnSimple } from 'getbasecore/Atoms';
-import { iconSuccess, iconDanger } from 'components/utils/images/images';
+import { Img, ProgressBar, BtnSimple, Iframe } from 'getbasecore/Atoms';
 import {
+  iconSuccess,
+  iconDanger,
   imgdefault,
   imgra,
   imgdolphin,
@@ -38,6 +38,7 @@ import {
 import sdlogo from 'assets/sdlogo.png';
 import remotelogo from 'assets/remotelogo.png';
 import amberlogo from 'assets/amberelec.jpg';
+
 const ipcChannel = window.electron.ipcRenderer;
 function End({
   disabledNext,
@@ -52,6 +53,8 @@ function End({
   message,
   percentage,
   onClickLog,
+  onClickWin32Config,
+  step,
 }) {
   const { state, setState } = useContext(GlobalContext);
   const { storage, installEmus, system } = state;
@@ -69,9 +72,9 @@ function End({
       (item) => item.status === true
     );
 
-    let bashArray = [];
+    const bashArray = [];
     onlySelectedEmus.forEach((item) => {
-      console.log(item.name);
+      // console.log(item.name);
       if (item.name === 'EmulationStation-DE') {
         item.name = 'ESDE';
       }
@@ -101,7 +104,7 @@ function End({
     ipcChannel.once('getEmuInstallStatus', (message) => {
       const emusChecked = message;
 
-      console.log(JSON.parse(message.stdout));
+      // console.log(JSON.parse(message.stdout));
       setStatePage({
         ...statePage,
         emusInstalledStatus: JSON.parse(message.stdout),
@@ -109,7 +112,7 @@ function End({
     });
   };
 
-  //We check if everything installed
+  // We check if everything installed
   useEffect(() => {
     if (system !== 'win32') {
       checkInstallation();
@@ -118,10 +121,10 @@ function End({
 
   return (
     <>
-      {disabledNext == true && <p className="lead">{message}...</p>}
+      {disabledNext === true && <p className="lead">{message}...</p>}
 
       <Main>
-        {disabledNext == false && (
+        {disabledNext === false && (
           <div className="tips">
             {system !== 'win32' && (
               <Card css="is-selected">
@@ -159,10 +162,96 @@ function End({
                 </div>
               </Card>
             )}
+            {system === 'win32' && step === undefined && (
+              <>
+                <Header title="Controller configuration" />
+                <p className="lead">
+                  You need to manually configure your controller in both some
+                  emulators and Steam. If you have done this already you can
+                  ignore this step.
+                </p>
+                <BtnSimple
+                  css="btn-simple--1"
+                  type="button"
+                  aria="Star manual configuration"
+                  onClick={() => onClickWin32Config()}
+                >
+                  Start controller configuration
+                  <svg
+                    className="rightarrow"
+                    width="32"
+                    height="32"
+                    viewBox="0 0 32 32"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M16.4091 8.48003L21.5024 13.5734L1.98242 13.5734L1.98242 18.0178H21.5024L16.4091 23.1111L19.5558 26.2578L30.018 15.7956L19.5558 5.33337L16.4091 8.48003Z"
+                    />
+                  </svg>
+                </BtnSimple>
+              </>
+            )}
+            {system === 'win32' && step === 'yuzu' && (
+              <>
+                <Header title="Yuzu configuration" />
+                <p className="lead">
+                  Click on the menu Emulation, Configure, Controls and select
+                  your controller in the Input Device dropdown.
+                  <br />
+                  Close Yuzu and continue so we can continue to configure the
+                  next emulator
+                </p>
+                <Iframe src="https://www.youtube-nocookie.com/embed/JvwdXTAmNWU?autoplay=1&playlist=JvwdXTAmNWU&loop=1&controls=0&mute=1&rel=0&modestbranding=1" />
+              </>
+            )}
+            {system === 'win32' && step === 'citra' && (
+              <>
+                <Header title="Citra configuration" />
+                <p className="lead">
+                  Click on the menu Emulation, Configure, Controls, click on
+                  Auto Map and follow the instructions.
+                  <br />
+                  Close Yuzu and continue so we can continue to configure the
+                  next emulator
+                </p>
+                <Iframe src="https://www.youtube-nocookie.com/embed/DRrjrR17wEs?autoplay=1&playlist=DRrjrR17wEs&loop=1&controls=0&mute=1&rel=0&modestbranding=1" />
+              </>
+            )}
+            {system === 'win32' && step === 'ryujinx' && (
+              <>
+                <Header title="Ryujinx configuration" />
+                <p className="lead">
+                  Click on the menu Options, Settings, Input, Player 1
+                  Configure, and select your controller in the Input Device
+                  dropdown
+                  <br />
+                  Close Ryujinx and continue so we can continue to configure the
+                  next emulator
+                </p>
+                <Iframe src="https://www.youtube-nocookie.com/embed/-THpQnpA1y8?autoplay=1&playlist=-THpQnpA1y8&loop=1&controls=0&mute=1&rel=0&modestbranding=1" />
+              </>
+            )}
+            {system === 'win32' && step === 'steam' && (
+              <>
+                <Header title="Steam configuration" />
+                <p className="lead">
+                  Open Steam by yourself, go to Settings, Controller, make sure
+                  Steam Input is enabled for your controller, scroll down to
+                  Desktop Layout, click on the current layout and then go to
+                  Templates and select the EmuDeck WE Template for your
+                  controller.
+                  <br />
+                  When you finish this step you can continue to the next page.
+                </p>
+                <Iframe src="https://www.youtube-nocookie.com/embed/ra_B1axeFqU?autoplay=1&playlist=ra_B1axeFqU&loop=1&controls=0&mute=1&rel=0&modestbranding=1" />
+              </>
+            )}
           </div>
         )}
         <br />
-        {disabledNext == true && (
+        {disabledNext === true && (
           <>
             <ProgressBar css="progress--success" value={percentage} max={100} />
             <span className="h5">
