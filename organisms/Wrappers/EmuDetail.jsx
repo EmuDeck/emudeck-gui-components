c8f4840671cfc2c71e11e44c447968f7c3e7d65e
Rodrigo <rodrigosedano@me.com>
2023-10-22 01:58:46 +0200

    Button Name Change

--- organisms/Wrappers/EmuDetail.jsx
+++ organisms/Wrappers/EmuDetail.jsx
@@ -341,7 +341,7 @@ function EmuDetail(props) {
                     onClickParsers();
                   }}
                 >
-                  Manage Parsers
+                  Standalone Parsers
                 </BtnSimple>
               )}
 
