dbd4c220807ee3d5039adb90b8b874cda90e3edb
Dragoon Dorise <rodrigosedano@me.com>
2024-04-21 22:44:18 +0200

    i18n

--- organisms/Wrappers/RomStorage.jsx
+++ organisms/Wrappers/RomStorage.jsx
@@ -28,83 +28,89 @@ function RomStorage({
   const { state } = useContext(GlobalContext);
   const { system } = state;
   return (
-    <Main>
-      <div className="cards">
-        {showSDCard && system !== 'darwin' && (
-          <Card
-            css={storage === 'SD-Card' && 'is-selected'}
-            onClick={() =>
-              sdCardValid === true ? onClick('SD-Card') : reloadSDcard()
-            }
-          >
-            <img src={imgSD} width="100" alt="Background" />
-            <span className="h5">SD Card</span>
-            {sdCardName != null && <span className="h6">{sdCardName}</span>}
-            {sdCardName === null ||
-              (sdCardValid === false && (
-                <span className="h6">
-                  Not detected
-                  <br />
-                  Click here to try again
-                </span>
-              ))}
-          </Card>
-        )}
-        {showInternal && (
-          <Card
-            css={storage === 'Internal Storage' && 'is-selected'}
-            onClick={() => onClick('Internal Storage')}
-          >
-            <img src={imgInternal} width="100" alt="Background" />
-            <span className="h5">Internal Storage</span>
-          </Card>
-        )}
-        {hddrives &&
-          hddrives.map((item) => {
-            if (item.letter === null) {
-            } else {
-              return (
-                <Card
-                  css={storage === `${item.letter}\\` && 'is-selected'}
-                  onClick={() => onClick(item.letter)}
-                >
-                  <img
-                    src={
-                      item.type === 'Internal'
-                        ? imgInternal
-                        : item.name.includes('card')
-                        ? imgSD
-                        : item.name.includes('Card')
-                        ? imgSD
-                        : item.type === 'External'
-                        ? imgExternal
-                        : imgNetwork
-                    }
-                    width="100"
-                    alt="Background"
-                  />
-                  <span className="h5">{`${item.letter}\\`}</span>
-                </Card>
-              );
-            }
-          })}
-        {showCustom && (
-          <Card
-            css={storage === 'Custom' && 'is-selected'}
-            onClick={() => onClick('Custom')}
-          >
-            <img src={imgInternal} width="100" alt="Background" />
-            <span className="h5">
-              {system === 'win32' && 'Custom Drive'}
-              {system !== 'win32' && 'Custom Directory'}
-            </span>
-            {customPath && storage === 'Custom' && (
-              <span className="h6">{customPath}</span>
-            )}
-          </Card>
-        )}
-      </div>
-    </Main>
+    <>
+      <p className="lead">
+        Your ROM directory will be squared away within an Emulation folder in
+        your selected directory.
+      </p>
+      <Main>
+        <div className="cards">
+          {showSDCard && system !== 'darwin' && (
+            <Card
+              css={storage === 'SD-Card' && 'is-selected'}
+              onClick={() =>
+                sdCardValid === true ? onClick('SD-Card') : reloadSDcard()
+              }
+            >
+              <img src={imgSD} width="100" alt="Background" />
+              <span className="h5">SD Card</span>
+              {sdCardName != null && <span className="h6">{sdCardName}</span>}
+              {sdCardName === null ||
+                (sdCardValid === false && (
+                  <span className="h6">
+                    Not detected
+                    <br />
+                    Click here to try again
+                  </span>
+                ))}
+            </Card>
+          )}
+          {showInternal && (
+            <Card
+              css={storage === 'Internal Storage' && 'is-selected'}
+              onClick={() => onClick('Internal Storage')}
+            >
+              <img src={imgInternal} width="100" alt="Background" />
+              <span className="h5">Internal Storage</span>
+            </Card>
+          )}
+          {hddrives &&
+            hddrives.map((item) => {
+              if (item.letter === null) {
+              } else {
+                return (
+                  <Card
+                    css={storage === `${item.letter}\\` && 'is-selected'}
+                    onClick={() => onClick(item.letter)}
+                  >
+                    <img
+                      src={
+                        item.type === 'Internal'
+                          ? imgInternal
+                          : item.name.includes('card')
+                          ? imgSD
+                          : item.name.includes('Card')
+                          ? imgSD
+                          : item.type === 'External'
+                          ? imgExternal
+                          : imgNetwork
+                      }
+                      width="100"
+                      alt="Background"
+                    />
+                    <span className="h5">{`${item.letter}\\`}</span>
+                  </Card>
+                );
+              }
+            })}
+          {showCustom && (
+            <Card
+              css={storage === 'Custom' && 'is-selected'}
+              onClick={() => onClick('Custom')}
+            >
+              <img src={imgInternal} width="100" alt="Background" />
+              <span className="h5">
+                {system === 'win32' && 'Custom Drive'}
+                {system !== 'win32' && 'Custom Directory'}
+              </span>
+              {customPath && storage === 'Custom' && (
+                <span className="h6">{customPath}</span>
+              )}
+            </Card>
+          )}
+        </div>
+      </Main>
+    </>
   );
 }
 
