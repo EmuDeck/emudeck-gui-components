6c86589212fda4b0fae7c81b3341b9f90f9ea8c7
Dragoon Dorise <rodrigosedano@me.com>
2025-01-27 22:33:49 +0100

    new checkbios

--- organisms/Wrappers/CheckBios.jsx
+++ organisms/Wrappers/CheckBios.jsx
@@ -15,6 +15,8 @@ function CheckBios({
   ps1Bios,
   ps2Bios,
   switchBios,
+  citronBios,
+  ryujinxBios,
   segaCDBios,
   saturnBios,
   dreamcastBios,
@@ -52,69 +54,102 @@ function CheckBios({
       <Main>
         <div className="container--grid">
           <div data-col-sm="6">
-            <Alert css={`alert--mini ${biosCSS(ps1Bios)}`}>
-              {biosText(ps1Bios).includes('missing') ? (
-                <Img src={iconDanger} css="icon icon--xs" alt="OK" />
-              ) : (
-                <Img src={iconSuccess} css="icon icon--xs" alt="OK" />
-              )}{' '}
-              Playstation 1 BIOS {biosText(ps1Bios)}
-            </Alert>
-            <Alert css={`alert--mini ${biosCSS(ps2Bios)}`}>
-              {biosText(ps2Bios).includes('missing') ? (
-                <Img src={iconDanger} css="icon icon--xs" alt="OK" />
-              ) : (
-                <Img src={iconSuccess} css="icon icon--xs" alt="OK" />
-              )}{' '}
-              Playstation 2 BIOS {biosText(ps2Bios)}
-            </Alert>
-            <Alert css={`alert--mini ${biosCSS(switchBios)}`}>
-              {biosText(switchBios).includes('missing') ? (
-                <Img src={iconDanger} css="icon icon--xs" alt="OK" />
-              ) : (
-                <Img src={iconSuccess} css="icon icon--xs" alt="OK" />
-              )}{' '}
-              Nintendo Switch Firmware {biosText(switchBios)}
-            </Alert>
-            <Alert css={`alert--mini ${biosCSS(segaCDBios)}`}>
-              {biosText(segaCDBios).includes('missing') ? (
-                <Img src={iconDanger} css="icon icon--xs" alt="OK" />
-              ) : (
-                <Img src={iconSuccess} css="icon icon--xs" alt="OK" />
-              )}{' '}
-              Sega CD BIOS {biosText(segaCDBios)}
-            </Alert>
-            <Alert css={`alert--mini ${biosCSS(saturnBios)}`}>
-              {biosText(saturnBios).includes('missing') ? (
-                <Img src={iconDanger} css="icon icon--xs" alt="OK" />
-              ) : (
-                <Img src={iconSuccess} css="icon icon--xs" alt="OK" />
-              )}{' '}
-              Saturn BIOS {biosText(saturnBios)}
-            </Alert>
-            <Alert css={`alert--mini ${biosCSS(DSBios)}`}>
-              {biosText(DSBios).includes('missing') ? (
-                <Img src={iconDanger} css="icon icon--xs" alt="OK" />
-              ) : (
-                <Img src={iconSuccess} css="icon icon--xs" alt="OK" />
-              )}{' '}
-              Nintendo DS BIOS {biosText(DSBios)}
-            </Alert>
-            <Alert
-              css={`alert--mini ${
-                dreamcastBios ? 'alert--success' : 'alert--warning'
-              }`}
-            >
-              {biosText(DSBios).includes('missing') ? (
-                <Img src={iconWarning} css="icon icon--xs" alt="OK" />
-              ) : (
-                <Img src={iconSuccess} css="icon icon--xs" alt="OK" />
-              )}{' '}
-              Dreamcast BIOS{' '}
-              {dreamcastBios ? t('CheckBios.detected') : t('CheckBios.missing')}
-            </Alert>
+            <div className="containera--grid">
+              <div data-col-sm="6">
+                <Alert css={`alert--mini ${biosCSS(ps1Bios)}`}>
+                  {biosText(ps1Bios).includes('missing') ? (
+                    <Img src={iconDanger} css="icon icon--xs" alt="OK" />
+                  ) : (
+                    <Img src={iconSuccess} css="icon icon--xs" alt="OK" />
+                  )}{' '}
+                  Playstation 1 BIOS {biosText(ps1Bios)}
+                </Alert>
+                <Alert css={`alert--mini ${biosCSS(ps2Bios)}`}>
+                  {biosText(ps2Bios).includes('missing') ? (
+                    <Img src={iconDanger} css="icon icon--xs" alt="OK" />
+                  ) : (
+                    <Img src={iconSuccess} css="icon icon--xs" alt="OK" />
+                  )}{' '}
+                  Playstation 2 BIOS {biosText(ps2Bios)}
+                </Alert>
+              </div>
+            </div>
+            <div className="container--grid">
+              <div data-col-sm="4">
+                <Alert css={`alert--mini ${biosCSS(switchBios)}`}>
+                  {biosText(switchBios).includes('missing') ? (
+                    <Img src={iconDanger} css="icon icon--xs" alt="OK" />
+                  ) : (
+                    <Img src={iconSuccess} css="icon icon--xs" alt="OK" />
+                  )}{' '}
+                  Yuzu Firmware {biosText(switchBios)}
+                </Alert>
+              </div>
+              <div data-col-sm="4">
+                <Alert css={`alert--mini ${biosCSS(ryujinxBios)}`}>
+                  {biosText(ryujinxBios).includes('missing') ? (
+                    <Img src={iconDanger} css="icon icon--xs" alt="OK" />
+                  ) : (
+                    <Img src={iconSuccess} css="icon icon--xs" alt="OK" />
+                  )}{' '}
+                  Ryujinx Firmware {biosText(ryujinxBios)}
+                </Alert>
+              </div>
+              <div data-col-sm="4">
+                <Alert css={`alert--mini ${biosCSS(citronBios)}`}>
+                  {biosText(citronBios).includes('missing') ? (
+                    <Img src={iconDanger} css="icon icon--xs" alt="OK" />
+                  ) : (
+                    <Img src={iconSuccess} css="icon icon--xs" alt="OK" />
+                  )}{' '}
+                  Citron Firmware {biosText(citronBios)}
+                </Alert>
+              </div>
+            </div>
+            <div className="containera--grid">
+              <div data-col-sm="6">
+                <Alert css={`alert--mini ${biosCSS(segaCDBios)}`}>
+                  {biosText(segaCDBios).includes('missing') ? (
+                    <Img src={iconDanger} css="icon icon--xs" alt="OK" />
+                  ) : (
+                    <Img src={iconSuccess} css="icon icon--xs" alt="OK" />
+                  )}{' '}
+                  Sega CD BIOS {biosText(segaCDBios)}
+                </Alert>
+                <Alert css={`alert--mini ${biosCSS(saturnBios)}`}>
+                  {biosText(saturnBios).includes('missing') ? (
+                    <Img src={iconDanger} css="icon icon--xs" alt="OK" />
+                  ) : (
+                    <Img src={iconSuccess} css="icon icon--xs" alt="OK" />
+                  )}{' '}
+                  Saturn BIOS {biosText(saturnBios)}
+                </Alert>
+                <Alert css={`alert--mini ${biosCSS(DSBios)}`}>
+                  {biosText(DSBios).includes('missing') ? (
+                    <Img src={iconDanger} css="icon icon--xs" alt="OK" />
+                  ) : (
+                    <Img src={iconSuccess} css="icon icon--xs" alt="OK" />
+                  )}{' '}
+                  Nintendo DS BIOS {biosText(DSBios)}
+                </Alert>
+                <Alert
+                  css={`alert--mini ${
+                    dreamcastBios ? 'alert--success' : 'alert--warning'
+                  }`}
+                >
+                  {biosText(DSBios).includes('missing') ? (
+                    <Img src={iconWarning} css="icon icon--xs" alt="OK" />
+                  ) : (
+                    <Img src={iconSuccess} css="icon icon--xs" alt="OK" />
+                  )}{' '}
+                  Dreamcast BIOS{' '}
+                  {dreamcastBios
+                    ? t('CheckBios.detected')
+                    : t('CheckBios.missing')}
+                </Alert>
+              </div>
+            </div>
           </div>
-
           <div data-col-sm="6">
             <ul className="list">
               <li>{t('CheckBios.tip1')}</li>
